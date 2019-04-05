278341c76c86f98e1fe1f9d385ca71421a755819
G.R.I.M <grim@tahitibob.home>
2019-04-05 14:04:59 +0200

    mise a jour test telecommande

--- sources/arduino/test-telecommande/test-telecommande.ino
+++ sources/arduino/test-telecommande/test-telecommande.ino
@@ -4,27 +4,17 @@
 //////////// PINS
 const int RECV_PIN = 2;
 
-/////////////////// VAR
-bool moteur_On = false;
-int vitesse;
 
 ////// INIT TELECOMMANDE
 IRrecv irrecv(RECV_PIN);
 decode_results results;
-////// INIT SERVO
-Servo myservo;  // Créer un objet servo
-
-
 
 //////////////////// SETUP
 void setup()
 {
   Serial.begin(9600);
   irrecv.enableIRIn(); // Initialise le recepteur
-  myservo.attach(9, 244, 5400); //Attache le servo (en fait l'ESC) au port 9
-  vitesse = vitesse_Initiale; //entre 90 et 180 multiple de 10 de preference
   
- 
 
 }
 
