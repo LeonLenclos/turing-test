46a1d01e60eac237cb7ce1c4651615cb249359bb
Léon Lenclos <leon.lenclos@gmail.com>
2019-04-15 10:14:38 +0200

    core local changes

--- sources/python/dispatch_table.py
+++ sources/python/dispatch_table.py
@@ -1,10 +1,15 @@
 from collections import namedtuple
 
-Directive = namedtuple('Directive', 'trig_in address osc_message clock_div')
+Directive = namedtuple('Directive', 'trig_in address osc_message')
 
 dispatch_table = [
-    Directive(trig_in=1, address=('ottomann.local', 4242), osc_message=('/debug', [1]), clock_div=1),
-    Directive(trig_in=2, address=('ottomann.local', 4242), osc_message=('/debug', [2]), clock_div=2),
-    Directive(trig_in=3, address=('ottomann.local', 4242), osc_message=('/debug', [3]), clock_div=4),
-    Directive(trig_in=4, address=('ottomann.local', 4242), osc_message=('/debug', [4]), clock_div=8),
+    Directive(trig_in=1, address=('ottomann.local', 4242), osc_message=('/debug', [1])),
+    Directive(trig_in=2, address=('ottomann.local', 4242), osc_message=('/debug', [2])),
+    Directive(trig_in=3, address=('ottomann.local', 4242), osc_message=('/debug', [3])),
+    Directive(trig_in=4, address=('ottomann.local', 4242), osc_message=('/debug', [4])),
+
+    Directive(trig_in=2, address=('lucy.local', 4242), osc_message=('/dance', [])),
+
+    Directive(trig_in=1, address=('godel.local', 4242), osc_message=('/clock', [])),
+    Directive(trig_in=3, address=('10.0.0.2', 50460), osc_message=('/control/navigator/run', ['default'])),
 ]
