bf1a5bf597db69f92a09ee5c2234ec3415a6c825
Léon Lenclos <leon.lenclos@gmail.com>
2019-04-16 12:47:16 +0200

    restore clock div

--- sources/python/core.py
+++ sources/python/core.py
@@ -9,6 +9,18 @@ from gpiozero import Button
 
 from dispatch_table import dispatch_table
 
+
+
+def log(msg):
+    """Write a message in the log file."""
+    print(msg)
+    print(msg, file=open("log", "a"))
+
+log("-"*50)
+log("CORE : new session ({})".format(datetime.datetime.now()))
+
+
+
 # keys are trig_index values are pin number
 trig_patch = {
     'GPIO4' :1,
@@ -16,17 +28,6 @@ trig_patch = {
     'GPIO22':3,
     'GPIO27':4,
 }
-
-def log(msg):
-    """
-    write a message in the log file
-    """
-    print(msg)
-    print(msg, file=open("/home/pi/log.txt", "a"))
-
-log("-"*50)
-log("{}".format(datetime.datetime.now()))
-log("CORE : new session")
 
 # `clients` is a dict containing all osc client.
 # Because we need one osc client by ip adress.
@@ -40,22 +41,24 @@ for directive in dispatch_table:
     if address not in clients:
         clients[address] = udp_client.SimpleUDPClient(*address)
 
+clock_counters = {k:0 for k in trig_patch}
+
 
 def on_trig(button):
     """
     Send the osc messages for all the directives that listen to trig_index
     """
     trig_index = trig_patch[repr(button.pin)]
-
-    log("{}".format(datetime.datetime.now()))
-    log('TRIG IN : {}'.format(trig_index))
+    clock_counters[trig_index] += 1
+    log('TRIG IN : {} ({})'.format(trig_index, clock_counters[trig_index]))
     for directive in dispatch_table:
         if directive.trig_in == trig_index:
-            clients[directive.address].send_message(*directive.osc_message)
-            log('\t-> OSC OUT : {osc_message} to {address}'.format(
-                osc_message=directive.osc_message,
-                address=directive.address)
-            )
+            if clock_counters[trig_index] % directive.clock_div == 0:
+                clients[directive.address].send_message(*directive.osc_message)
+                log('\t-> OSC OUT : {osc_message} to {address}'.format(
+                    osc_message=directive.osc_message,
+                    address=directive.address)
+                )
 
 
 buttons = {}
@@ -63,7 +66,7 @@ buttons = {}
 for pin, trig_index in trig_patch.items():
     log('PATCH: trig {} on GPIO{}'.format(trig_index, pin))
     buttons[trig_index] = Button(pin)
-    buttons[trig_index].when_pressed = on_trig
+    buttons[trig_index].when_released = on_trig
     # buttons[trig_index].when_released = lambda : on_trig(trig_index)
 
 
