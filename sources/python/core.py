edfb59495723d0cf12435ba3e0cd4c93a71d1e34
Léon Lenclos <leon.lenclos@gmail.com>
2019-04-17 16:42:58 +0200

    update core

--- sources/python/core.py
+++ sources/python/core.py
@@ -16,10 +16,9 @@ def log(msg):
     print(msg)
     print(msg, file=open("log", "a"))
 
+
 log("-"*50)
 log("CORE : new session ({})".format(datetime.datetime.now()))
-
-
 
 # keys are trig_index values are pin number
 trig_patch = {
@@ -32,43 +31,38 @@ trig_patch = {
 # `clients` is a dict containing all osc client.
 # Because we need one osc client by ip adress.
 # So, keys are (ip, port) and values are SimpleUDPClient(ip, port)
-clients = {}
+osc_clients = {}
 
 log("DISPATCH TABLE :")
 for directive in dispatch_table:
     log("\t- {}".format(directive))
-    address = directive.address
-    if address not in clients:
-        clients[address] = udp_client.SimpleUDPClient(*address)
-
-clock_counters = {trig_index:0 for _, trig_index in trig_patch.items()}
+    address = directive.hostname, directive.port
+    if directive.scheme == 'osc' and address not in osc_clients:
+        osc_clients[address] = udp_client.SimpleUDPClient(*address)
 
 
 def on_trig(button):
     """
-    Send the osc messages for all the directives that listen to trig_index
+    Do the directives.
     """
     trig_index = trig_patch[repr(button.pin)]
-    clock_counters[trig_index] += 1
-    log('TRIG IN : {} ({})'.format(trig_index, clock_counters[trig_index]))
+    log('TRIG IN : {}'.format(trig_index))
     for directive in dispatch_table:
         if directive.trig_in == trig_index:
-            if clock_counters[trig_index] % directive.clock_div == 0:
-                clients[directive.address].send_message(*directive.osc_message)
-                log('\t-> OSC OUT : {osc_message} to {address}'.format(
-                    osc_message=directive.osc_message,
-                    address=directive.address)
-                )
-
+            if directive.clock_div.clock():
+                log('\t> doing {}'.format(directive))
+                if directive.netloc == 'osc':
+                    osc_clients[directive.address]
+                    .send_message(*directive.content)
+                elif directive.netloc == 'http' :
+                    requests.post(directive.url, json=directive.content)
 
 buttons = {}
 
 for pin, trig_index in trig_patch.items():
-    log('PATCH: trig {} on GPIO{}'.format(trig_index, pin))
+    log('PATCH: trig {} on {}'.format(trig_index, pin))
     buttons[trig_index] = Button(pin)
     buttons[trig_index].when_released = on_trig
-    # buttons[trig_index].when_released = lambda : on_trig(trig_index)
-
 
 log('START SERVING...')
 
