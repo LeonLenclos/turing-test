d87d09956a84ac2210bf99a7769440d478c7fb0d
Léon Lenclos <leon.lenclos@gmail.com>
2019-09-24 11:33:19 +0200

    threading core

--- sources/python/core.py
+++ sources/python/core.py
@@ -1,25 +1,17 @@
 #! /usr/bin/python3.5
-
+    
 import datetime
+from threading import Thread
 from collections import OrderedDict
+
 from pythonosc import osc_message_builder
 from pythonosc import udp_client
 from signal import pause
 from gpiozero import Button
-
 from dispatch_table import dispatch_table
 
 import requests
-session = requests.session()
 
-def log(msg):
-    """Write a message in the log file."""
-    print(msg)
-    print(msg, file=open("/home/pi/log", "a"))
-
-
-log("-"*50)
-log("CORE : new session ({})".format(datetime.datetime.now()))
 
 # keys are trig_index values are pin number
 trig_patch = {
@@ -29,45 +21,68 @@ trig_patch = {
     'GPIO27':4,
 }
 
-# `clients` is a dict containing all osc client.
-# Because we need one osc client by ip adress.
-# So, keys are (ip, port) and values are SimpleUDPClient(ip, port)
-osc_clients = {}
-
-log("DISPATCH TABLE :")
-for directive in dispatch_table:
-    log("\t- {}".format(directive))
-    address = directive.hostname, directive.port
-    if directive.scheme == 'osc' and directive.netloc not in osc_clients:
-        osc_clients[directive.netloc] = udp_client.SimpleUDPClient(directive.hostname, directive.port)
 
 
-def on_trig(button):
-    """
-    Do the directives.
-    """
-    trig_index = trig_patch[repr(button.pin)]
-    log('TRIG IN : {}'.format(trig_index))
-    for directive in dispatch_table:
-        if directive.trig_in == trig_index:
-            if directive.clock_div.clock():
-                log('\t> doing {}'.format(directive))
-                try:
-                    if directive.scheme == 'osc':
-                        osc_clients[directive.netloc].send_message(directive.path, directive.content)
-                    elif directive.scheme == 'http':
-                        session.post(directive.url, json=directive.content)
-                    else :
-                        raise Exception('Core dont know the {} protocole'.format(directive.scheme))
-                except Exception as e:
-                    log('\tERROR ! {}'.format(e))
-buttons = {}
 
-for pin, trig_index in trig_patch.items():
-    log('PATCH: trig {} on {}'.format(trig_index, pin))
-    buttons[trig_index] = Button(pin)
-    buttons[trig_index].when_released = on_trig
 
-log('START SERVING...')
 
-pause()
+class TrigHandler(Thread):
+
+    def __init__(self, table):
+        Thread.__init__(self)
+        self.table = table
+        self.session = requests.session()
+        self.log("-"*50)
+        self.log("CORE : new session ({})".format(datetime.datetime.now()))
+
+        # `clients` is a dict containing all osc client.
+        # Because we need one osc client by ip adress.
+        # So, keys are (ip, port) and values are SimpleUDPClient(ip, port)
+        self.osc_clients = {}
+        self.log("DISPATCH TABLE :")
+        for directive in self.table:
+            self.log("\t- {}".format(directive))
+            address = directive.hostname, directive.port
+            if directive.scheme == 'osc' and directive.netloc not in self.osc_clients:
+                self.osc_clients[directive.netloc] = udp_client.SimpleUDPClient(directive.hostname, directive.port)
+
+
+        self.buttons = {}
+        for pin, trig_index in trig_patch.items():
+            self.log('PATCH: trig {} on {}'.format(trig_index, pin))
+            self.buttons[trig_index] = Button(pin)
+            self.buttons[trig_index].when_released = self.on_trig
+
+    def run(self):
+        self.log('START SERVING...')
+        pause()
+
+    def log(self, msg):
+        """Write a message in the log file."""
+        print(msg)
+        print(msg, file=open("/home/pi/log", "a"))
+    
+    def on_trig(self, button):
+        """
+        Do the directives.
+        """
+        trig_index = trig_patch[repr(button.pin)]
+        self.log('TRIG IN : {}'.format(trig_index))
+        for directive in self.table:
+            if directive.trig_in == trig_index:
+                if directive.clock_div.clock():
+                    self.log('\t> doing {}'.format(directive))
+                    try:
+                        if directive.scheme == 'osc':
+                            self.osc_clients[directive.netloc].send_message(directive.path, directive.content)
+                        elif directive.scheme == 'http':
+                            self.session.post(directive.url, json=directive.content)
+                        else :
+                            raise Exception('Core dont know the {} protocole'.format(directive.scheme))
+                    except Exception as e:
+                        self.log('\tERROR ! {}'.format(e))
+
+
+thread1 = TrigHandler(dispatch_table)
+thread1.start()
+thread1.join()
